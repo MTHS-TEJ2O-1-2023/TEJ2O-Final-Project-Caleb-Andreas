c8a4f402a725d0aa1e941f43c00e360bf0bc00b9
Caleb Andreas <caleb.andreas@mths.ca>
2025-01-08 11:25:55 -0500

    started code

--- main.ts
+++ main.ts
@@ -1,8 +1,31 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Caleb Andreas
+ * Created on: Jan 2025
+ * This program rotates a specific stepper motor depending on the signal it receives.
 */
 
-basic.showString('Hello, World!')
+// Variables.
+let leftTrack = 0
+let rightTrack = 0
+
+// Set group, cleanup then happy face at start.
+radio.setGroup(149)
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function() {
+    if (leftTrack == 0) {
+        leftTrack = leftTrack + 1
+        radio.sendNumber(leftTrack = leftTrack + 1)
+        basic.clearScreen()
+        basic.showString('L on')
+        basic.showIcon(IconNames.Happy)
+    } else {
+        leftTrack = leftTrack - 1
+        basic.clearScreen()
+        radio.sendNumber(leftTrack = leftTrack - 1)
+        basic.showString('L off')
+        basic.showIcon(IconNames.Happy)
+    }
+})
